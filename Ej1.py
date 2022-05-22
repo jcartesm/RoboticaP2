db6046466e707c14298572d9e82f32b1fee119b7
Jorge Conejeros <jorgeconejerosrios@gmail.com>
2022-05-22 01:12:54 -0400

    Walking Robot

--- Ej1.py
+++ Ej1.py
@@ -1,5 +1,7 @@
 import matplotlib as plt
 import math as mt
+import pygame
+import random as ra
 
 nEstados    = 9
 nIter       = 1000 # Iteracions
@@ -9,7 +11,7 @@ k           = 1
 aDir        = ["N","S","E","O"] # Direccion 
 JOptimo     = [[0 for i in range(0,nEstados)]] # Jota Optimo (J*)
 aP          = ["?" for i in range(0,nEstados)] # Acciones a tomar en estado
-RecxAcc     = [0,0,0,0] # Reward x Accion
+RwdxAcc     = [0,0,0,0] # Reward x Accion
 nError      = 0 # Error
 aRec        = [] # matriz con las Iteraciones
 nMax        = 0 # Maximo
@@ -103,30 +105,30 @@ for i in range(1,nIter):
             nV3 += mTEste[s][j]  *  aRec[i-1][j]  #mt.exp(-nDesc * aRwd[j])*aRec[i-1][j]  # Este
             nV4 += mTOeste[s][j] *  aRec[i-1][j]  #mt.exp(-nDesc * aRwd[j])*aRec[i-1][j] # Oeste
             
-        RecxAcc[0] = aRwd[s] + (nV1 * Lamda)  
-        RecxAcc[1] = aRwd[s] + (nV2 * Lamda)
-        RecxAcc[2] = aRwd[s] + (nV3 * Lamda) 
-        RecxAcc[3] = aRwd[s] + (nV4 * Lamda) 
+        RwdxAcc[0] = aRwd[s] + (nV1 * Lamda)  
+        RwdxAcc[1] = aRwd[s] + (nV2 * Lamda)
+        RwdxAcc[2] = aRwd[s] + (nV3 * Lamda) 
+        RwdxAcc[3] = aRwd[s] + (nV4 * Lamda) 
         
-        aRec[i][s] = max(RecxAcc)
+        aRec[i][s] = max(RwdxAcc)
         
         nV1 = 0 ; nV2 = 0 
         nV3 = 0 ; nV4 = 0 
         
-        print(RecxAcc)
-        nMax = max(RecxAcc)
-        nPos = RecxAcc.index(nMax)
+        #print(RwdxAcc)
+        nMax = max(RwdxAcc)
+        nPos = RwdxAcc.index(nMax)
         aP[s] = aDir[nPos]
 
     nError = Norma(aRec[i],aRec[i-1])
     #print("Recorrido en i = ",aRec[i])
-    print(nError)
+    #print(nError)
     if nError < e: # almacenar el J* Optimo
         for k in range(0,nEstados):
             JOptimo[0][k] = aRec[i][k]
         print(" k optimo  = ", i , " , error => ", nError, " , J* Optimo => ",JOptimo)
         break
-
+print(aP)
 # Calculo de la Mejor Accion a tomar en los Estados S(i) segun JOptimo (J*).-
 for s in range(0,nEstados):
     for j in range(0,nEstados):
@@ -135,16 +137,16 @@ for s in range(0,nEstados):
         nV3 += mTEste[s][j]  * JOptimo[0][j]
         nV4 += mTOeste[s][j] * JOptimo[0][j]
    
-    RecxAcc[0] = aRwd[s] + (nV1 * Lamda) # Accion 1-Norte
-    RecxAcc[1] = aRwd[s] + (nV2 * Lamda) # Accion 2-Sur
-    RecxAcc[2] = aRwd[s] + (nV3 * Lamda) # Accion 3-Este
-    RecxAcc[3] = aRwd[s] + (nV4 * Lamda) # Accion 4-Oeste
+    RwdxAcc[0] = aRwd[s] + (nV1 * Lamda) # Accion 1-Norte
+    RwdxAcc[1] = aRwd[s] + (nV2 * Lamda) # Accion 2-Sur
+    RwdxAcc[2] = aRwd[s] + (nV3 * Lamda) # Accion 3-Este
+    RwdxAcc[3] = aRwd[s] + (nV4 * Lamda) # Accion 4-Oeste
 
     nV1 = 0; nV2 = 0; nV3 = 0; nV4 = 0 
     #print 'Estado[%d] Reward Accion [N]: %0.3f Reward Accion [S]: %0.3f Reward Accion [E]: %0.3f Reward Accion [O]: %0.3f' %(s,nA[0],nA[1],nA[2],nA[3])   
 
-    nMax = max(RecxAcc)
-    nPos = RecxAcc.index(nMax)
+    nMax = max(RwdxAcc)
+    nPos = RwdxAcc.index(nMax)
     aP[s] = aDir[nPos]
 
     
@@ -153,4 +155,108 @@ print('-'*87,
 for s in range(0,nEstados):
  print ("En S_"+str(s)," => ",aP[s])    
 print('='*87)
-        
+
+######################################################################################
+
+#                              PYGAME SIMULATION
+
+######################################################################################
+
+# Seteo de la ventana
+pygame.init()
+ventana = pygame.display.set_mode((875, 500))
+pygame.display.set_caption('Value Iteration')
+WHITE = (255, 255, 255)
+ventana.fill(WHITE)
+fondo = pygame.image.load("img/fondo.png")
+fondo = pygame.transform.scale(fondo, [875, 500])
+ventana.blit(fondo,[0,0])
+
+pygame.display.flip()
+
+# Cargado de Imagenes a ocupar
+robot = pygame.image.load("img/robot.png")
+robot = pygame.transform.scale(robot, [50, 50])
+
+casilla = pygame.image.load("img/normal.png")
+casilla = pygame.transform.scale(casilla, [125, 125])
+
+casilla_win = pygame.image.load("img/win.png")
+casilla_win = pygame.transform.scale(casilla_win, [125, 125])
+
+casilla_bad = pygame.image.load("img/lose.png")
+casilla_bad = pygame.transform.scale(casilla_bad, [125, 125])
+
+def RestFondo():
+    ventana.fill(WHITE)
+    ventana.blit(fondo,[0,0])
+    for i in range(0,7):  
+        if i == 2:
+            ventana.blit(casilla_bad,[125*i,50])
+        if i == 4:
+            ventana.blit(casilla_bad,[125*i,300])  
+        if i == 0 or i == 6:
+            ventana.blit(casilla_win,[125*i,175])
+        else:
+            ventana.blit(casilla,[125*i,175])
+
+def Mover(posActual,mov):
+    x = posActual[0]
+    y = posActual[1]
+    if mov == "N":
+        for i in range(25):
+            y = y - 5
+            if y < 210:
+                return [x,y+5]
+            RestFondo()
+            ventana.blit(robot,[x,y])
+            pygame.display.update()                            
+            pygame.time.delay(40)
+        return [x,y]
+    if mov == "S":
+        for i in range(25):
+            y = y + 5
+            if y > 210:
+                return [x,y-5]
+            RestFondo()
+            ventana.blit(robot,[x,y])
+            pygame.display.update()                            
+            pygame.time.delay(40)
+        return [x,y]
+    if mov == "E":
+        for i in range(25):
+            x = x + 5
+            RestFondo()
+            ventana.blit(robot,[x,y])
+            pygame.display.update()                            
+            pygame.time.delay(40)
+        return [x,y]
+    if mov == "O":
+        for i in range(25):
+            x = x - 5
+            RestFondo()
+            ventana.blit(robot,[x,y])
+            pygame.display.update()                            
+            pygame.time.delay(40)
+        return [x,y]
+    
+RestFondo()
+
+# Posicion inicial aleatoria del robot
+Estado_Robot = [[35,210],[160,210],[285,210],[285,85],[410,210],[535,210],[535,335],[660,210],[785,210]]
+indexIni =  ra.randint(2,6)
+posRobot = Estado_Robot[indexIni]
+ventana.blit(robot,[posRobot[0],posRobot[1]])
+
+    
+pygame.display.update()
+
+is_running = True
+while is_running:
+    posRobot = Mover(posRobot,aP[indexIni])
+    indexIni = Estado_Robot.index(posRobot)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+pygame.quit()
